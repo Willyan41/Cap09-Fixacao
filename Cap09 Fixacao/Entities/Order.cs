a868a57a2b99d4cbef02109ecece603a20070f0b
Willyan Santos <willyan41@hotmail.com>
2020-04-11 11:15:28 -0300

    Adicionar arquivos de projeto.

---
+++ Cap09 Fixacao/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using Cap09_Fixacao.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Cap09_Fixacao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItems(OrderItem items)
+        {
+            Items.Add(items);
+        }
+
+        public void RemoveItems(OrderItem items)
+        {
+            Items.Remove(items);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem items in Items)
+            {
+                sum += items.SubTotal();
+
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ Cap09 Fixacao/Program.cs
@@ -0,0 +1,60 @@
+﻿using Cap09_Fixacao.Entities;
+using Cap09_Fixacao.Enums;
+using System;
+using System.Globalization;
+
+namespace Cap09_Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data:");
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantityProduct = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, priceProduct);
+
+                OrderItem orderItem = new OrderItem(quantityProduct, priceProduct, product);
+
+
+                order.AddItems(orderItem);
+
+            }
+
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order);
+
+        }
+    }
+}
